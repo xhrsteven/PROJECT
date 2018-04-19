d98f2517c36747060a2e3afd65ce0ba95bf52590
Steven <xhrsteven@gmail.com>
2018-04-19 01:38:47 -0500

    update no cross join error on my end

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,10 @@
 $('#searchBar').hide();
-
 $('#searchConcerts').on('click', function() {
     $('#searchBar').show('slow');
     $('#searchConcerts').hide();
     var logo = $('#logo');
     logo.attr('style', 'width: 100px');
 })
-
 $('#submit').on('click', function(event) {
     event.preventDefault();
     $('#carousel').hide('slow');
@@ -16,70 +14,48 @@ $('#submit').on('click', function(event) {
     var priceMax = $('#price-max').val().trim();
     var city = $('#city').val().trim();
     
-
     var newUrl = "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" + userInput + "&limit=5&prices=" +  priceMin + "," + priceMax + "&city=" + city +  "&apikey=6AHBYWAdSZSjLGGMe3pKvtKLiPPGneD9";
-    
 
+    
 $.ajax({
     type:"GET",
     url: newUrl,
     async:true,
     dataType: "json",
     success: function(json) {
-        console.log(json._embedded);
-            
-
-            
-            
+        console.log(json._embedded);   
             for (i = 0; i < 5; i++) {
-                (function(ii){
-                    setTimeout(function(){
-                        console.log(ii);
-                    },1000);
-                
-
-
-                // var longitude = (json._embedded.events[i]._embedded.venues[0].location.longitude);
-                // var latitude = (json._embedded.events[i]._embedded.venues[0].location.latitude);
             
-                var eventImage = $('.eventImage');
-                eventImage.attr('style', 'width:fit-content');
-            a = $('#link');
+            var eventImage = $('.eventImage');
+            eventImage.attr('style', 'width:fit-content');
+            var a = $('#link');
             a.attr('href', 'details.html');
             
             var img = $('<img>');
-            img.attr('data-id', json._embedded.events[i].id);
+            img.attr('data-index', json._embedded.events[i].id);
             img.attr('src', json._embedded.events[i].images[0].url);
-            img.attr('data-id', json._embedded.events[i].id);
             img.attr('style', 'width: 300px; height: 200px; margin-left: 10px; clear: both;');
             img.attr('class', 'imageBox');
-
             var imgName = $('<p>' + json._embedded.events[i].name + '</p><p>' + json._embedded.events[i]._embedded.venues[0].name + '</p><p>' + json._embedded.events[i].dates.start.localDate + '</p><p>' + json._embedded.events[i].dates.start.localTime + '</p>');
-            imgName.attr('style', 'line-height: 40%; margin-top: 5px')
             
+            imgName.attr('style', 'line-height: 40%; margin-top: 5px')
             eventImage.append(img);
             eventImage.append(imgName);
             
-
-
-            
             img.on("click", function() {
-            localStorage.clear();
-            let idPush = img.attr("data-id");
-            localStorage.setItem("ID", JSON.stringify(idPush));
-        })
-
-    })(i);
+                localStorage.clear();
+              var idPush = img.attr("data-index");
+            //   list.push(idPush); 
+              localStorage.setItem("id", JSON.stringify(idPush));
+              window.location = "details.html";
+            });
+          
         }
+              
              },
              
     error: function(xhr, status, err) {
                 // This time, we do not end up here!
              }
             });
-
-
-
         });
-
-
